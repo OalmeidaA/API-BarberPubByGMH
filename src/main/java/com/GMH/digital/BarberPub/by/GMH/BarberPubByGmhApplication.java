89146f01d279cbf4bdaf014c83786c7b7abed0b6
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-05-24 17:24:01 -0300

    Project Created

---
+++ src/main/java/com/GMH/digital/BarberPub/by/GMH/BarberPubByGmhApplication.java
@@ -0,0 +1,13 @@
+package com.GMH.digital.BarberPub.by.GMH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberPubByGmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberPubByGmhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GMH/digital/BarberPub/by/GMH/BarberPubByGmhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GMH.digital.BarberPub.by.GMH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberPubByGmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
